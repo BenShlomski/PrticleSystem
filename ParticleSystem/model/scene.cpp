ebbe65a61b3f222d28c6c27573a2b114e38d0e85
John Maoz <therainbowcat6@gmail.com>
2023-05-15 22:55:19 +0300

    changed image heights

--- ParticleSystem/model/scene.cpp
+++ ParticleSystem/model/scene.cpp
@@ -15,14 +15,14 @@ Scene::Scene() {
 	// TODO find light color and scale settings. also some way that is not absoulutly awful to set the material.
 	BrightbillboardCreateInfo lightInfo;
 	lightInfo.color = glm::vec3(1, 1, 1);
-	lightInfo.position = glm::vec3(1, 0, 0);
+	lightInfo.position = glm::vec3(1, 0, 1.0);
 	lightInfo.strength = 5.0f;
 	lights.push_back(new BrightBillboard(&lightInfo));
 	lightInfo.color = glm::vec3(0, 1, 0);
-	lightInfo.position = glm::vec3(3, 2, 0);
+	lightInfo.position = glm::vec3(3, 2, 1.0);
 	lights.push_back(new BrightBillboard(&lightInfo));
 	lightInfo.color = glm::vec3(1, 1, 1);
-	lightInfo.position = glm::vec3(3, 0, 0);
+	lightInfo.position = glm::vec3(3, 0, 1.0);
 	lights.push_back(new BrightBillboard(&lightInfo));
 
 	//medkits.push_back(new Billboard(glm::vec3(2.5f, 0.0f, 0.5f)));
